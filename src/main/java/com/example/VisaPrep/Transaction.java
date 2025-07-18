d63f45fc5f0705de0b35e0c9bc691ab9981f3ba4
dinojazvin <jazvind@gmail.com>
2025-07-18 14:38:37 -0700

    Initial Commit

---
+++ src/main/java/com/example/VisaPrep/Transaction.java
@@ -0,0 +1,78 @@
+package com.example.VisaPrep;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    private String currency;
+    private String description;
+    private LocalDateTime timestamp;
+    private String idempotencyKey;
+
+    public Transaction(){}
+
+    public String getIdempotencyKey() {
+        return idempotencyKey;
+    }
+
+    public void setIdempotencyKey(String idempotencyKey) {
+        this.idempotencyKey = idempotencyKey;
+    }
+
+    public Transaction(BigDecimal amount, String currency, String description, LocalDateTime timestamp) {
+        this.amount = amount;
+        this.currency = currency;
+        this.description = description;
+        this.timestamp = timestamp;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
---
+++ src/main/java/com/example/VisaPrep/TransactionController.java
@@ -0,0 +1,59 @@
+package com.example.VisaPrep;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @GetMapping
+    public List<Transaction> getAllTransactions(){
+        return transactionRepository.findAll();
+    }
+
+    @GetMapping("/id")
+    public Optional<Transaction> getTransactionById(@PathVariable Long id){
+        return transactionRepository.findById(id);
+    }
+
+    @PostMapping
+    public Transaction createTransaction(@RequestBody Transaction transaction){
+        String idempotencyKey = transaction.getIdempotencyKey();
+
+        if (idempotencyKey == null || idempotencyKey.trim().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "idempotnecy key required");
+        }
+
+        Optional<Transaction> existing = transactionRepository.findByIdempotencyKey(idempotencyKey);
+        return existing.orElseGet(() -> transactionRepository.save(transaction));
+
+    }
+
+    @PutMapping("/{id}")
+    public Transaction updateTransaction(@PathVariable Long id, @RequestBody Transaction transactionDetails) {
+        return transactionRepository.findById(id)
+                .map(transaction -> {
+                    transaction.setAmount(transactionDetails.getAmount());
+                    transaction.setCurrency(transactionDetails.getCurrency());
+                    transaction.setDescription(transactionDetails.getDescription());
+                    transaction.setTimestamp(transactionDetails.getTimestamp());
+                    return transactionRepository.save(transaction);
+                })
+                .orElseThrow(() -> new RuntimeException("Transaction not found with id" + id));
+    }
+
+    @DeleteMapping
+    public void deleteTransaction(@PathVariable Long id){
+        transactionRepository.deleteById(id);
+    }
+
+}
